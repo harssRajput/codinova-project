05fb5c766b3f12c8143bb3cfebb9a06d832c1799
Harsh Rajput <hrrajput20@gmail.com>
2021-05-21 17:33:33 +0530

    add productStock and Cart components

---
+++ src/components/Cart.js
@@ -0,0 +1,10 @@
+
+function Cart() {
+  return (
+    <div className="cart">
+      this is cart container
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/components/ProductStock.js
@@ -0,0 +1,11 @@
+
+function ProductStock() {
+    return (
+      <div className="product-stock">
+        this is product stock container
+      </div>
+    );
+  }
+  
+  export default ProductStock;
+  
