f63427dc15136fae299296ce29b920601497fe21
Harsh Rajput <hrrajput20@gmail.com>
2021-05-21 22:42:13 +0530

    add basic style to footer

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,17 @@
-import CartItem from './CartItem';
-import Footer from './Footer';
+import CartItem from "./CartItem";
+import Footer from "./Footer";
 
 function Cart() {
   return (
     <div className="cart">
-      this is cart container
-      <CartItem/>
-      <Footer/>
+      <div className="cart-header">
+        <span className="products">PRODUCTS</span>
+        <span className="price">PRICE</span>
+        <span className="quantity">QUANTITY</span>
+        <span className="total">TOTAL</span>
+      </div>
+      <CartItem />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,43 @@
-
 function Footer() {
-    return (
-      <div className="footer">
-        this is Footer container
-      </div>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <table className="footer">
+      <tr className="subtotal footer-child">
+        <td className="left">SubTotal</td>
+        <td className="right">
+          <span>0.000EUR</span>
+          <span>0 items</span>
+        </td>
+      </tr>
+      <tr className="VAT-tax footer-child">
+        <td className="left">VAT tax</td>
+        <td className="right">
+          <span>10 %</span>
+          <span>0.000EUR</span>
+        </td>
+      </tr>
+      <tr className="discount footer-child">
+        <td className="left">Discount</td>
+        <td className="right">
+          <span>10 %</span>
+          <span>0.000EUR</span>
+        </td>
+      </tr>
+      <tr className="total footer-child">
+        <td className="left">Total</td>
+        <td className="right">
+          <span>0.000EUR</span>
+        </td>
+      </tr>
+      <tr className="buttons footer-child">
+        <td className="left">
+          <button className="cancel-all">CANCEL ALL</button>
+        </td>
+        <td className="right">
+          <button className="process-all">PROCESS ALL</button>
+        </td>
+      </tr>
+    </table>
+  );
+}
+
+export default Footer;
