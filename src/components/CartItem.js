5e5d4a2f318597943b41089096add54e32990941
Harsh Rajput <hrrajput20@gmail.com>
2021-05-23 15:34:44 +0530

    add total feature n cart-items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,72 +1,55 @@
 import React from "react";
 
 
-class CartItem extends React.Component {
-
-  constructor(){
-    super();
-
-    this.state={
-      total: 0,
-    }
-  }
-  
-  setTotal = (product)=> {
-    this.setState({ total: this.props.product.price,})
-  }
-
-  render() {
-    // console.log("price of product ", typeof(this.props.product.price));
-    const { name, price, qty } = this.props.product;
-    const {
-      onIncreaseQuantity,
-      onDecreaseQuantity,
-      onDeleteItem,
-      product,
-    } = this.props;
-    // this.setTotal(product);
-    return (
-      <tr className="cart-item">
-        <td>
+function CartItem(props) {
+  const { name, price, qty } = props.product;
+  const {
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteItem,
+    product,
+  } = props;
+  return (
+    <tr className="cart-item">
+      <td>
+        <button
+          onClick={() => {
+            onDeleteItem(product);
+          }}
+        >
+          x
+        </button>
+      </td>
+      <td>
+        <span>{name}</span>
+      </td>
+      <td>
+        <span>{price}</span>
+      </td>
+      <td>
+        <div>
           <button
             onClick={() => {
-              onDeleteItem(product);
+              onDecreaseQuantity(product);
             }}
           >
-            x
+            -
           </button>
-        </td>
-        <td>
-          <span>{name}</span>
-        </td>
-        <td>
-          <span>{price}</span>
-        </td>
-        <td>
-          <div>
-            <button
-              onClick={() => {
-                onDecreaseQuantity(product);
-              }}
-            >
-              -
-            </button>
-            <span>{qty}</span>
-            <button
-              onClick={() => {
-                onIncreaseQuantity(product);
-              }}
-            >
-              +
-            </button>
-          </div>
-        </td>
-        <td>
-          <span className="total">{price*qty}</span>
-        </td>
-      </tr>
-    );
-  }
+          <span>{qty}</span>
+          <button
+            onClick={() => {
+              onIncreaseQuantity(product);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </td>
+      <td>
+        <span className="total">{price*qty}</span>
+      </td>
+    </tr>
+  );
 }
 
 export default CartItem;
